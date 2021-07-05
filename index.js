b189cbbe66fd6b51a1557273403c3d0762b65dce
danilbek <deni200040@gmail.com>
2021-07-05 13:32:35 +0300

    Изменил модель Client, именил файл 'gitignore' и добавил новые модели 'Note' и 'Status'

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const cors = require("cors")
-const mongoose = require("mongoose")
-require('dotenv').config()
-const { urlencoded } = require('express')
-const port = process.env.PORT || 5000
-const DB_URL = process.env.DB_URL
-const app = express()
- 
-app.use(cors())
-app.use(urlencoded({extended: true}))
-app.use(express.json())
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+require('dotenv').config();
+const { urlencoded } = require('express');
+
+const { PORT, DB_URL } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan('combined'));
 
 async function start() {
-    try {
-        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: true, useCreateIndex: true})
-        app.listen(3000, () => console.log("Сервер запущен"))
-        
-    } catch(e) {
-        console.log(e.message);
-    }
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
-start()
+start();
